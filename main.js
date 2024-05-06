55476d083d3a2c65b9c6766ff2ea9e3d03b240a7
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-06 17:32:18 +0530

    implemented

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const filePath = process.argv[2];
 
 function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         reject(`Error reading file ${filePath}: ${err}`);
         return;
@@ -16,7 +16,11 @@ function readFileAsync(filePath) {
 
 function modifyText(text) {
   return new Promise((resolve, reject) => {
-    // TODO: Convert text to uppercase and reverse it
+    // Convert text to uppercase
+    const upperCaseText = text.toUpperCase();
+    // Reverse the text
+    const reversedText = upperCaseText.split("").reverse().join("");
+    resolve(reversedText);
   });
 }
 
